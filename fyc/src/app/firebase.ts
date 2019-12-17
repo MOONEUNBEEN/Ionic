812bc171443056e907f5b1c0b9e64f5d1607d2a0
MOONEUNBEEN <e_bb@daum.net>
2019-12-17 21:56:56 +0900

    FInd Your Color
    Kim JuEun
    Ji EunSoo
    Monn EunBeen

---
+++ fyc/src/app/firebase.ts
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyAWFZn0R-SwHV_VPoETDFm9HxmM2cQOn8Y",
+    authDomain: "findyourcolor-49af9.firebaseapp.com",
+    databaseURL: "https://findyourcolor-49af9.firebaseio.com",
+    projectId: "findyourcolor-49af9",
+    storageBucket: "findyourcolor-49af9.appspot.com",
+    messagingSenderId: "978538420885",
+    appId: "1:978538420885:web:fce4981bc1e79f70"
+};
+
+export default config
